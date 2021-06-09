ba409043d82d51deccb9e5a2156c2f7f2f573bef
samuel.guimaraes <samuel.guimaraes@cabal.com.br>
2021-06-08 21:31:46 -0300

    Implementa QuickSort para ordenar produtos pelo preço efetivo

---
+++ src/main/java/br/com/rchlo/domain/QuickSort.java
@@ -0,0 +1,43 @@
+package br.com.rchlo.domain;
+
+public class QuickSort {
+	
+	public static void ordena(final Produto[] produtos, final int de, final int ate) {
+		int elementos = ate - de;
+		if (elementos > 1) {
+			int posicaoDoPivo = particiona(produtos, de, ate);
+			
+			ordena(produtos, de, posicaoDoPivo);
+			ordena(produtos, posicaoDoPivo + 1, ate);
+		}
+	}
+	
+	private static int particiona(final Produto[] produtos, int inicial, int termino) {
+		
+		int menoresEncontrados = 0;
+		
+		Produto pivo = produtos[termino - 1];
+		
+		for (int analisando = inicial; analisando < termino - 1; analisando++) {
+			Produto atual = produtos[analisando];
+			
+			if (pivo.getPrecoEfetivo().compareTo(atual.getPrecoEfetivo()) > 0) {
+				troca(produtos, analisando, menoresEncontrados);
+				menoresEncontrados++;
+			}
+		}
+		troca(produtos, termino - 1, menoresEncontrados);
+		
+		return menoresEncontrados;
+	}
+	
+	private static void troca(final Produto[] produtos, final int de, final int para) {
+		
+		Produto nota1 = produtos[de];
+		Produto nota2 = produtos[para];
+		produtos[para] = nota1;
+		produtos[de] = nota2;
+		
+	}
+	
+}
